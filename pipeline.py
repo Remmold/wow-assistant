960475d790c276c538b6b380ae1ad35f19c0cdf3
vegetablecloud <ludvig.johansson@hotmail.com>
2025-06-13 10:48:56 +0200

    FEAT: Added a pipeline file with a function that runs the pipeline

---
+++ pipeline.py
@@ -0,0 +1,14 @@
+import dlt
+from resources import wow_ah_source
+
+def run_pipeline():
+    pipeline = dlt.pipeline(
+    pipeline_name="wow_ah",  # The name of the pipeline
+    destination="duckdb",  # The destination where the data will be loaded
+    dataset_name="raw"  # The name of the dataset
+    )
+    load_info = pipeline.run(wow_ah_source())
+    print(load_info)
+
+if __name__ == "__main__":
+    run_pipeline()
