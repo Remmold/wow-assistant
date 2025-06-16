6b2650eadf87e6363e24803914e00413acd87fff
vegetablecloud <ludvig.johansson@hotmail.com>
2025-06-16 22:13:46 +0200

    FEAT: Add commodities resource to extract non-realm-based auction items for broader dataset coverage in future MVP price trend analysis. * Updated all resources to yield rows individually (streaming) instead of in batches for better performance.

--- wow_ah_dlt/resources.py
+++ wow_ah_dlt/resources.py
@@ -16,13 +16,12 @@ def _fetch_ids():
     
     list_of_realm_ids = [x["href"].split("/")[-1].split("?")[0] for x in connected_realm_list]
     return list_of_realm_ids
-REALM_IDS = _fetch_ids()
 
 # Fetch ah
 @dlt.resource(table_name="auctions", write_disposition="replace")
 def fetch_auction_house_items():
     counter = 0 #DEBUG
-    for realm_id in REALM_IDS[:10]: # 92
+    for realm_id in _fetch_ids(): 
         try:
             endpoint = f"/data/wow/connected-realm/{realm_id}/auctions"                             
             params = {                
@@ -35,14 +34,16 @@ def fetch_auction_house_items():
             data = response.json()
             for auction in data["auctions"]:
                 auction["realm_id"] = realm_id
-            yield data["auctions"]
+                yield auction # Detta tar rad för rad så i varje connected realm, så yieldar man istället rad för rad istället för alla cirka 30-50 k rader
+            # yield data["auctions"] < Detta blir batchar man yieldar med 30 k rader svårt för dlt att behandla
         except Exception as e:
             print(f"DENNA MISSLYCKADES nr{counter} realm id: {realm_id}")
         print(f"\nYIELDAT NR {counter} <-------------------\n") #DEBUG
-    
+
+# Fetch connected realm data    
 @dlt.resource(table_name="realm_data", write_disposition="replace")
 def fetch_realm_data():
-    for realm_id in REALM_IDS:
+    for realm_id in _fetch_ids():
         endpoint = f"/data/wow/connected-realm/{realm_id}"
         params = {                
             "{{connectedRealmId}}" : realm_id,
@@ -58,7 +59,15 @@ def fetch_realm_data():
 # Fetch ah commodities
 @dlt.resource(table_name="commodities", write_disposition="replace") # merge?
 def fetch_ah_commodities():
-    pass
+    endpoint = f"/data/wow/auctions/commodities"                             
+    params = {                
+        "namespace" : "dynamic-eu",
+        }
+    response = auth_util.get_api_response(endpoint=endpoint, params=params)
+    data = response.json()
+    for auction in data["auctions"]:
+        yield auction
+        
 
 # Returns a list of indexes for item classes
 def fetch_item_classes(): 
@@ -148,10 +157,6 @@ def fetch_items():
     return all_items
 
 
-
-
-
-
 # @dlt.source that we use in pipeline.run instead of @dlt.resource we use all the resources we want to run in the pipeline
 @dlt.source(name="wow_ah")
 def wow_ah_source():
@@ -159,10 +164,9 @@ def wow_ah_source():
     This is the source function that will be used in the pipeline.
     It returns all the resources that we want to run in the pipeline.
     """
-    return [fetch_items(),fetch_auction_house_items(),fetch_realm_data()]
-
+    return [fetch_items(),fetch_auction_house_items(),fetch_ah_commodities(),fetch_realm_data()]
 
 
 if __name__ == "__main__":
-    fetch_item_subclasses()
+    fetch_ah_commodities()
 
