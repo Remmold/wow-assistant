dd2f0cc59b1dc8f4ceadf8521519ba5ee32c83cd
vegetablecloud <ludvig.johansson@hotmail.com>
2025-06-16 22:11:41 +0200

    FEAT: Added progress = 'alive_progess' to easier monitor the Extract/Load steps in the pipeline. This is a 'Work in progress'

--- wow_ah_dlt/pipeline.py
+++ wow_ah_dlt/pipeline.py
@@ -5,7 +5,8 @@ def run_pipeline():
     pipeline = dlt.pipeline(
     pipeline_name="wow_api_data",  # The name of the pipeline
     destination="duckdb",  # The destination where the data will be loaded
-    dataset_name="raw"  # The name of the dataset
+    dataset_name="raw",  # The name of the dataset
+    progress="alive_progress"
     )
     load_info = pipeline.run(wow_ah_source())
     print(load_info)
