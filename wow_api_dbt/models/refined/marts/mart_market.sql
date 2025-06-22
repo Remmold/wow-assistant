b53ed64bbe23647140f1d8836b509ebd9970a724
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-06-22 17:04:03 +0200

    Fixed query

---
+++ wow_api_dbt/models/refined/marts/mart_market.sql
@@ -0,0 +1,35 @@
+-- models/marts/mart_market.sql
+
+
+WITH mart_market AS (
+    SELECT
+		fm.realm_group_id,
+		dr.realm_name,
+		di.name AS item_name,
+		da.bid,
+		da.buyout,
+		da.quantity,
+		da.time_left,
+		di.rarity_name,
+		di.media_id,
+		di.vendor_sell_price,
+		di.vendor_purchase_price,
+		di.item_class_name,
+		di.item_subclass_name,
+		da.battlepet_breed_id,
+		da.battlepet_level,
+		da.battlepet_quality_id,
+		da.battlepet_species,
+FROM {{ref('fct_market')}} AS fm
+
+LEFT JOIN {{ref(('dim_auctions'))}} AS da
+	ON da.id = fm.auction_id
+
+LEFT JOIN {{ref('dim_items')}} AS di
+	ON di.id = fm.item_id
+
+LEFT JOIN {{ref('dim_realms')}} AS dr
+	ON dr.realm_group_id = fm.realm_group_id
+    )
+
+SELECT * FROM mart_market
