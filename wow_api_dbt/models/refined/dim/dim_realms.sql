b5a01ce00113ea58277f032d9a81d754c9b76d5c
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-06-22 16:22:20 +0200

    Updated dim_realms

--- wow_api_dbt/models/refined/dim/dim_realms.sql
+++ wow_api_dbt/models/refined/dim/dim_realms.sql
@@ -3,18 +3,17 @@
 WITH connected_realms AS (
   SELECT 
     realm_data.id AS realm_group_id,
-    rdr.id AS id,
-    rdr.name__en_us AS name,
-    rdr.category__en_us AS category,
+    rdr.id AS realm_id,
+    rdr.name__en_us AS realm_name,
+    rdr.region__id AS region_id,
+    rdr.region__name__en_us AS region_name,
+    rdr.category__en_us AS language,
     rdr.timezone,
     rdr.type__name__en_us AS type,
-    realm_data.has_queue,
-    realm_data.status__name__en_us AS server_status,
     realm_data.population__name__en_us AS population_status,
+    realm_data.status__name__en_us AS server_status,
+    realm_data.has_queue,
     realm_data.mythic_leaderboards__href,
-    realm_data.auctions__href AS auctions_href,
-    rdr.region__name__en_us AS region_name,
-    rdr.region__id AS region_id,
     rdr.is_tournament
   FROM {{ source('raw', 'realm_data') }} AS realm_data
   JOIN {{ source('raw', 'realm_data__realms') }} AS rdr
