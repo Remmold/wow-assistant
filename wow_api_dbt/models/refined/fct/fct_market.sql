f79969cb0628c2450ee42e6ecf3fa0d5cf24b986
Daniel Wahlgren <dwahlgren93@gmail.com>
2025-06-22 17:04:37 +0200

    Fixed incorrect field selection

--- wow_api_dbt/models/refined/fct/fct_market.sql
+++ wow_api_dbt/models/refined/fct/fct_market.sql
@@ -8,12 +8,12 @@ WITH market_data AS (
         a.id AS auction_id,
         i.id AS item_id,
         --c.id AS commodity_id,
-        r.connected_realm_id AS connected_realm_id,
-        r.id as realm_id
+        r.realm_group_id
     FROM {{ ref('dim_auctions') }} AS a
+    
     LEFT JOIN {{ ref('dim_items')}} AS i ON i.id = a.item_id
     --JOIN {{ ref('dim_commodities')}} AS c ON c.item_id = i.id
-    LEFT JOIN {{ ref('dim_realms')}} AS r ON r.connected_realm_id = a.realm_id
+    LEFT JOIN {{ ref('dim_realms')}} AS r ON r.realm_group_id = a.realm_group_id
 )
 
 SELECT * FROM market_data
