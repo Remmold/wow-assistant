0b483aa00e454a89a99d66db31bce8fdf8bcb1ee
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-07-01 11:48:35 +0200

    Added Testmode for pipeline

--- wow_api_dlt/pipeline.py
+++ wow_api_dlt/pipeline.py
@@ -2,15 +2,26 @@ import dlt
 from resources import wow_api_source
 
 DB_PATH = "wow_api_dbt/wow_api_data.duckdb"
+DB_TEST_PATH = "wow_api_dbt/test_wow_api_data.duckdb"
 
-def run_pipeline():
-    pipeline = dlt.pipeline(
-    pipeline_name="wow_api_data",  # The name of the pipeline
-    destination=dlt.destinations.duckdb(str(DB_PATH)),  # The destination where the data will be loaded
-    dataset_name="raw",  # The name of the dataset
-    progress="alive_progress"
-    )
-    load_info = pipeline.run(wow_api_source())
+def run_pipeline(testmode=False):
+    if testmode:
+        pipeline = dlt.pipeline(
+        pipeline_name="wow_api_data_test",  # The name of the pipeline for test mode
+        destination=dlt.destinations.duckdb(str(DB_TEST_PATH)),  # The destination where the data will be loaded
+        dataset_name="test",  # The name of the dataset for test mode
+        progress="log"  # The progress reporting mode, can be "log", "console", or "none"
+        )    
+        load_info = pipeline.run(wow_api_source(test_mode=True))  # Run the pipeline with test mode enabled
+    else:
+        pipeline = dlt.pipeline(
+        pipeline_name="wow_api_data",  # The name of the pipeline
+        destination=dlt.destinations.duckdb(str(DB_PATH)),  # The destination where the data will be loaded
+        dataset_name="raw",  # The name of the dataset
+        progress="log" # The progress reporting mode, can be "log", "console", or "none"
+        )
+
+        load_info = pipeline.run(wow_api_source())
     print(load_info)
 
 if __name__ == "__main__":
