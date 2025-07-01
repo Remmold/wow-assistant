397aee3ffef5b653aa2f5c0fb6e2cdb231f11ffe
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-07-01 16:03:33 +0200

    Added functional diffrences between a testrun and a regular full run aswell as adding support for running specific resources

--- wow_api_dlt/pipeline.py
+++ wow_api_dlt/pipeline.py
@@ -1,28 +1,23 @@
 import dlt
-from resources import wow_api_source
+from .resources import wow_api_source
 
 DB_PATH = "wow_api_dbt/wow_api_data.duckdb"
 DB_TEST_PATH = "wow_api_dbt/test_wow_api_data.duckdb"
 
-def run_pipeline(testmode=False):
-    if testmode:
-        pipeline = dlt.pipeline(
-        pipeline_name="wow_api_data_test",  # The name of the pipeline for test mode
-        destination=dlt.destinations.duckdb(str(DB_TEST_PATH)),  # The destination where the data will be loaded
-        dataset_name="test",  # The name of the dataset for test mode
-        progress="log"  # The progress reporting mode, can be "log", "console", or "none"
-        )    
-        load_info = pipeline.run(wow_api_source(test_mode=True))  # Run the pipeline with test mode enabled
+def run_pipeline(sources = None, test_mode=False):
+    pipeline = dlt.pipeline(
+    pipeline_name="wow_api_data",  # The name of the pipeline for test mode
+    destination=dlt.destinations.duckdb(str(DB_TEST_PATH)),  # The destination where the data will be loaded
+    dataset_name="raw",  # The name of the dataset for test mode
+    progress="log"  # The progress reporting mode, can be "log", "console", or "none"
+    )    
+    if sources is not None:
+        # If a specific source list is provided, we use it to run only those resources
+        load_info = pipeline.run(wow_api_source(optional_source_list=sources,test_mode=test_mode))
     else:
-        pipeline = dlt.pipeline(
-        pipeline_name="wow_api_data",  # The name of the pipeline
-        destination=dlt.destinations.duckdb(str(DB_PATH)),  # The destination where the data will be loaded
-        dataset_name="raw",  # The name of the dataset
-        progress="log" # The progress reporting mode, can be "log", "console", or "none"
-        )
-
-        load_info = pipeline.run(wow_api_source())
-    print(load_info)
+        load_info = pipeline.run(wow_api_source(test_mode=test_mode))
+    if load_info:    
+        print(load_info)
 
 if __name__ == "__main__":
     run_pipeline()
